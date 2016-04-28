9b41cbadbea4635f0d43fff400d57f007e897092
Tristan Seifert <t@tseifert.me>
2016-04-28 11:46:04 -0500

    Initial Commit

---
+++ Avocado/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Avocado
+//
+//  Created by Tristan Seifert on 20160428.
+//  Copyright © 2016 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ Avocado/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Avocado
+//
+//  Created by Tristan Seifert on 20160428.
+//  Copyright © 2016 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
